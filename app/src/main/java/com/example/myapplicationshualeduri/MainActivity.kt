ec8d7c3c25c52e59fb3384c2e6243d373f7d501a
Tatamindiashvili <tamar.mindiashvili.1@btu.edu.ge>
2021-11-06 18:23:45 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationshualeduri/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplicationshualeduri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import android.widget.Button
+    import android.widget.CheckBox
+    import android.widget.Toast
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
